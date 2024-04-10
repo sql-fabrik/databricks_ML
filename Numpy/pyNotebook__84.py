069ba63a6b147318f23cc1eb462663fee71b1897
git4schulung@sql-fabrik.de <git4schulung@sql-fabrik.de>
2024-04-10 14:04:03 +0000

    Exkurs   plt.plot & plt.scatter

---
+++ Numpy/pyNotebook__84.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+## Exkurs: Daten plotten
+%matplotlib inline  ##-- zeigt das Chart "im REsultbereich des Notebooks"
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [6, 7, 8])
+plt.show()
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+import numpy as np
+xs = np.arange(-3, 3, 0.1)
+ys = xs ** 3 - xs ** 2 + 7
+
+plt.plot(xs, ys)
+plt.show()
+
+# COMMAND ----------
+
+plt.scatter(xs, ys)
+plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # databricks_ML
+
+databricks Runtime 13.3 LTS (Scala 2.12, Spark 3.4.1)
+Photon Acceleration
+----
